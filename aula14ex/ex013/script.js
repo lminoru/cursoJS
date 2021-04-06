61edb4488e2cb10582cf957e8a87b033347988e8
Leonardo Minoru Iwashima <CIANDT\liwashima@lnb028452cps.ciandt.global>
2021-04-06 17:40:30 -0300

    tudo

--- aula14ex/ex013/script.js
+++ aula14ex/ex013/script.js
@@ -1,1 +1,19 @@
-alert("Salve!");
+function gerarTabuada(){
+    const num = document.getElementById('txtnum').value;
+    const seltab = document.querySelector('div #seltab');
+
+
+    seltab.innerHTML = '';
+    if (num.length == 0){
+        alert("Digite um número");
+        seltab.innerHTML = '<option>Digite um número acima</option>';
+    } else
+    {
+        const n = Number(num);
+
+        for (let i=1; i<=10 ; i++){
+            seltab.innerHTML += `<option>${n} x ${i} = ${n*i}</option>`;
+        }
+    }
+    
+}
---
+++ aula15/ambinte.js
@@ -0,0 +1,4 @@
+let num = [5,2,8,3]    
+
+for (let pos=0; )
+console.log(num);
